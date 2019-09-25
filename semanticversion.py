6ac6be04ae145e157bfc4bdf891b7234f1ce02ae
junpei-oyama <junpei.oyama.ai@gmail.com>
2019-09-25 23:26:45 +0900

    【課題D-3】パッチバージョンアップをできるようにした。
    - patch_version_upが呼ばれたらpatchを1つインクリメントしてselfを返すようにした。

--- semanticversion.py
+++ semanticversion.py
@@ -10,6 +10,10 @@ class SemanticVersion:
     def __str__(self):
         return f'{self.major}.{self.minor}.{self.patch}'
 
+    def patch_version_up(self):
+        self.patch += 1
+        return self
+
 
 def main():
     py370 = SemanticVersion(major=3, minor=7, patch=0)
@@ -26,6 +30,16 @@ def main():
     py370 = SemanticVersion(major=3, minor=7, patch=0)
 
     print('3.7.0' == str(py370))  # True
+
+    print()
+
+    # Python3.7.0 というバージョンを表現したもの
+    py370 = SemanticVersion(major=3, minor=7, patch=0)
+
+    py371 = py370.patch_version_up()
+    print(SemanticVersion(3, 7, 1) == py371)  # True
+
+    print()
 
 
 if __name__ == '__main__':
