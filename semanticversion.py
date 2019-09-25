56b02a89d2b69d4683a3780a4eb8061e665aa235
junpei-oyama <junpei.oyama.ai@gmail.com>
2019-09-25 17:44:33 +0900

    【課題D-2】文字列表現の追加
    - __str__を用いて文字列表現を取得できるようにした。

--- semanticversion.py
+++ semanticversion.py
@@ -7,6 +7,9 @@ class SemanticVersion:
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
+    def __str__(self):
+        return f'{self.major}.{self.minor}.{self.patch}'
+
 
 def main():
     py370 = SemanticVersion(major=3, minor=7, patch=0)
@@ -16,6 +19,13 @@ def main():
 
     # 等価でない場合
     print(SemanticVersion(3, 7, 1) == py370)  # False
+
+    print()
+
+    # Python3.7.0というバージョンを表現したもの
+    py370 = SemanticVersion(major=3, minor=7, patch=0)
+
+    print('3.7.0' == str(py370))  # True
 
 
 if __name__ == '__main__':
